268210a4592fea9a247ff7a7b97a46f01782b288
IvLaptev <ivlaptev13@ya.ru>
2022-11-06 13:33:39 +0300

    Adds docs to method

--- documents/app/api/endpoints/documents_router.py
+++ documents/app/api/endpoints/documents_router.py
@@ -1,10 +1,12 @@
+from typing import List
 from fastapi import APIRouter, Depends
 
 from app.services.documents_service import DocumentsService
+from app.models.document import Document
 
 
 router = APIRouter()
 
-@router.get('/')
+@router.get('/', response_model=List[Document], description="Returns all documents with pagination")
 def get_documents(doc_server: DocumentsService = Depends()):
     return doc_server.get_all_documents()
